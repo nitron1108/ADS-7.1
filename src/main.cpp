1374b812cc6a0d636618f4a65828d02c99c5147b
nitron1108 <mja29122@gmail.com>
2026-06-09 00:01:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,42 @@
 // Copyright 2022 NNTU-CS
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
+  srand (time (nullptr));
+  for (int n = 10; n <= 500; n += 10){
+    //все выключены
+    Train trainOff;
+    for (int i = 0; i < n; i++){
+      trainOff.addCar(false);
+    }
+    trainOff.getLength();
+    int offOps = trainOff.getOpCount();
 
-  while (count--)
-    train.addCar(false);
+    //все включены
+    Train trainOn;
+    for (int i = 0; i < n; i++){
+      trainOn.addCar(true);
+    }
+    trainOn.getLength();
+    int onOps = trainOn.getOpCount();
 
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
+    //случайным образом
+    Train trainRandoms;
+    for (int i = 0; i < n; i++){
+      trainRandoms.addCar(rand() % 2);
+    }
+    trainRandoms.getLength();
+    int randomsOps = trainRandoms.getOpCount();
+
+    std::cout
+      << n << " "
+      << offOps << " "
+      << onOps << " "
+      << randomsOps << std::endl;  
+  }
   return 0;
 }
