31ffd062539c379f9b0029767c38ebd26b601034
nitron1108 <mja29122@gmail.com>
2026-06-08 23:11:03 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,55 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2022 NNTU-CS
 #include "train.h"
+
+Train::Train() : entryPoint(nullptr), stepCounter(0) {}
+
+void Train::addWagon(bool lampState) {
+  Wagon* fresh = new Wagon{lampState, nullptr, nullptr};
+  if (!entryPoint) {
+    entryPoint = fresh;
+    fresh->next = fresh;
+    fresh->prev = fresh;
+  } else {
+    Wagon* tail = entryPoint->prev;
+    fresh->next = entryPoint;
+    fresh->prev = tail;
+    tail->next = fresh;
+    entryPoint->prev = fresh;
+  }
+}
+
+int Train::calculateLength() {
+  if (!entryPoint) return 0;
+
+  stepCounter = 0;
+  Wagon* current = entryPoint;
+
+  if (!current->lamp) current->lamp = true;
+
+  while (true) {
+    int forwardSteps = 0;
+    Wagon* runner = current;
+
+    do {
+      runner = runner->next;
+      ++forwardSteps;
+      ++stepCounter;
+    } while (!runner->lamp);
+
+    runner->lamp = false;
+
+    const Wagon* backtrack = runner;
+    for (int i = 0; i < forwardSteps; ++i) {
+      backtrack = backtrack->prev;
+      ++stepCounter;
+    }
+
+    if (backtrack == current && !backtrack->lamp) {
+      return forwardSteps;
+    }
+  }
+}
+
+int Train::getStepCount() const {
+  return stepCounter;
+}
